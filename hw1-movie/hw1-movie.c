2c4fe7ff473b6d4a28dd676aaa7a2b59e6181ba1
Srikar Valluri <srikarvalluri@Srikars-MacBook-Pro.local>
2021-01-18 20:31:32 -0800

    finalized assignment 1, with comments

--- hw1-movie/hw1-movie.c
+++ hw1-movie/hw1-movie.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* struct for movie information */
+/* struct for important movie information */
 struct movie
 {
     char *title;
@@ -114,12 +114,19 @@ void printMovie(struct movie *aMovie)
            aMovie->rating);
 }
 
+/*
+Print data specifically for Option 2's formatting (year, rating, title)
+*/
 void printOption2(struct movie *aMovie)
 {
     printf("%s %s %s\n", aMovie->year,
            aMovie->rating,
            aMovie->title);
 }
+
+/*
+Print data specifically for Option 3's formatting (year, title)
+*/
 
 void printOption3(struct movie *aMovie)
 {
@@ -129,7 +136,7 @@ void printOption3(struct movie *aMovie)
 
 
 /*
-* Print the linked list of movies
+* Print the struct attributes of a specific movie
 */
 void printMovieList(struct movie *list)
 {
@@ -140,6 +147,9 @@ void printMovieList(struct movie *list)
     }
 }
 
+/*
+Print the entire linked list, and the attributes of each struct in the list
+*/
 int returnMovieCount(struct movie *list)
 {
     int i = 0;
@@ -158,6 +168,10 @@ int returnMovieCount(struct movie *list)
 *       gcc --std=gnu99 -o movies main.c
 */
 
+/*
+This is the main input/output interface. It shows the options using printf
+It then uses scanf to recieve a guaranteed integer input, which will then be returned. 
+*/
 int inputOption()
 {
     int inputNumber;
@@ -170,121 +184,163 @@ int inputOption()
     return inputNumber;
 }
 
+/*
+If the user inputs 1, then they will be led to this function
+This function takes input from the user, and selectively detects which movies (structs) match with that year
+Information is printed in the console
+*/
+
 void optionOne(struct movie *list)
 {
     int yearRequested;
-    int numMovies = 0;
-    printf("Enter for the year for which you want to see movies: ");
-    scanf("%d", &yearRequested);
-    while(list != NULL)
+    int numMovies = 0; //count for determining whether movies were even counted that year
+    printf("Enter for the year for which you want to see movies: "); //user input message
+    scanf("%d", &yearRequested); //user input
+    while(list != NULL) //looping through the movie list
     {
-        if(yearRequested == atoi(list->year))
+        if(yearRequested == atoi(list->year)) //determining whether the year matched with the input request
         {
             printf("%s\n", list->title);
             numMovies++;
         }
         list = list->next;
     }
-    if(numMovies == 0)
+    if(numMovies == 0) //no movies matched with the year input condition
     {
         printf("No data about movies released in %d\n", yearRequested);
     }
     
 }
 
+/*
+If the user inputs 2, then they will be led to this function
+This function uses a stack system. As we iterate through the list, we find the best possible values for each year.
+We then update an array (sometimes replacing) values in the array, so that for every year, there is only one movie struct within the array, with the highest rating.
+Information is printed in the console
+*/
+
 void optionTwo(struct movie *list)
 {
-    struct movie *minMovies[121] = { NULL };
-    int counter;
-    while(list != NULL)
+    struct movie *minMovies[121] = { NULL }; //array that contains the highest-rank structs
+    int counter; //counter
+    while(list != NULL) //iterating through list
     {
-        counter = 0;
-        while(1)
+        counter = 0; //initialize counter
+        while(1) //loop that runs forever, but stops with break statements
         {
-            if(minMovies[counter] != NULL)
+            if(minMovies[counter] != NULL) //if there are structs already in the array
             {
-                if(atoi(list->year) == atoi(minMovies[counter]->year))
+                if(atoi(list->year) == atoi(minMovies[counter]->year)) //compare the year of the current list with the struct in the array
                 {
-                    if(strtod(list->rating, NULL) >= strtod(minMovies[counter]->rating, NULL))
+                    if(strtod(list->rating, NULL) >= strtod(minMovies[counter]->rating, NULL)) //if they are the same year, compare the rating
                     {
-                        minMovies[counter] = list;
+                        minMovies[counter] = list; //replace the old struct with new struct if rating is higher, we want max rating for all years
                     }
-                    break;
+                    break; //now that we've iterated and replaced, there's no point of continuing to iterate, so break
                 }
                 
             }
-            else
+            else //if there is no structs in the array
             {
-                minMovies[counter] = list;
-                break;
+                minMovies[counter] = list; //add the first struct, it's arbitrary
+                break; //break, don't loop through a list with nulls
             }
-            counter++;
+            counter++; //iterate through the array to check for new stored structs 
         }
-        list = list->next;
+        list = list->next; //iterate through the linked list
     }
-    counter = 0;
-    while(minMovies[counter] != NULL)
+    counter = 0; //reset counter
+    while(minMovies[counter] != NULL) //iterate through the array
     {
-        printOption2(minMovies[counter]);
-        counter++;
+        printOption2(minMovies[counter]); //print the neccessary information with the highest ratings, with part 2's required formatting
+        counter++; //iterate through the array
     }
 
 }
 
+/*
+If the user inputs 3, they require all the movies for a specific language. After an input is taken, we modify the input with a "tag" (aka an underscore before and after)
+We then modify the struct movie-> languages to replace any characters other than alphabets to an underscore as well.
+This way, we can determine whether a string subset will uniquely identify as a language within the string of all languages
+
+*/
+
 void optionThree(struct movie *list)
 {
-    char languageRequested[20];
-    int numMovies = 0;
-    printf("Enter the language for which you want to see movies: ");
-    scanf("%s", languageRequested);
-    while(list != NULL)
+    char languageRequested[20]; //original user input
+    char modLanguageRequested[22]; //modified user input
+    char actualCharacter[100]; //modified struct movie->languages with _
+    char *pointerChar; //pointer to iterate through characters
+    int indexChar; //index to index the pointer
+    int length; //number of characters
+    int numMovies = 0; //check to determine whether there even were movies that were in a particular language
+    printf("Enter the language for which you want to see movies: "); //language user input message
+    scanf("%s", languageRequested); //language user input
+    snprintf(modLanguageRequested, sizeof modLanguageRequested, "_%s_", languageRequested); //modifying original input
+
+    while(list != NULL) //iterate through list
     {
-        if(strstr(list->languages, languageRequested) != NULL)
+        indexChar = 0; //initialize index
+        pointerChar = list->languages; //pointer that points to languages
+        length = strlen(pointerChar); //determining char length of languages
+        while(indexChar < length) //iterating through the chars of the language string, parsing anything with [ ; or ], replacing it with _
+        {
+            if(pointerChar[indexChar] == '[' || pointerChar[indexChar] == ';' || pointerChar[indexChar] == ']')
+            {
+                actualCharacter[indexChar] = '_';
+            }
+            else
+            {
+                actualCharacter[indexChar] = pointerChar[indexChar]; //if it's part of the alphabet, then just directly copy to new array
+            }
+            indexChar++;
+        }
+        if(strstr(actualCharacter, modLanguageRequested) != NULL) //see if modified input is within the modified language string, if so, then same with non-modified counterparts
         {
             printOption3(list);
             numMovies++;
         }
+
         list = list->next;
     }
-    if(numMovies == 0)
+    if(numMovies == 0) //alternate case of no language match 
     {
         printf("No data about movies released in %s\n", languageRequested);
     }
 }
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
+    if (argc < 2) //provide easy data input
     {
         printf("You must provide the name of the file to process\n");
         printf("Example usage: ./movies movies_sample_1.csv\n");
         return EXIT_FAILURE;
     }
-    struct movie *list = processFile(argv[1]);
-    printf("Processed file %s and parsed data for %i movies\n", argv[1], returnMovieCount(list));
-    // printMovieList(list);
-    int inputNumber = 0;
+    struct movie *list = processFile(argv[1]); //creating linked list
+    printf("Processed file %s and parsed data for %i movies\n", argv[1], returnMovieCount(list)); //sending message to user to confirm success of linked list creation
+    int inputNumber = 0; //recieving input value
     while(1)
     {
-        inputNumber = inputOption();
-        if(inputNumber == 1)
+        inputNumber = inputOption(); //input function, clean input
+        if(inputNumber == 1) // user chooses option 1, call function that does option 1 task
         {
             optionOne(list);
         }
-        else if(inputNumber == 2)
+        else if(inputNumber == 2) // user chooses option 2, call function that does option 2 task
         {
             optionTwo(list);
         }
-        else if(inputNumber == 3)
+        else if(inputNumber == 3) // user chooses option 3, call function that does option 3 task
         {
             optionThree(list);
         }
-        else if(inputNumber == 4)
+        else if(inputNumber == 4) // user chooses option 4, call function that does option 4 task (break/exit)
         {
             break;
         }
         else
         {
-            printf("You entered an incorrect choice. Try again.\n");
+            printf("You entered an incorrect choice. Try again.\n"); // if integer is not within range
         }
     }
 
