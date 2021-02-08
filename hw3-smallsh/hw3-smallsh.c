2999ac46b8647ae2784ff4cef2b13b927dc95e3d
Srikar Valluri <valluris@os1.engr.oregonstate.edu>
2021-02-08 12:23:39 -0800

    don't accidentally dlete pls

--- hw3-smallsh/hw3-smallsh.c
+++ hw3-smallsh/hw3-smallsh.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>  // rand(), srand()
 #include <time.h>    // time()
 #include <fcntl.h>
+#include <signal.h>
+
 
 
 struct inOut 
@@ -20,6 +22,35 @@ struct inOut initialize()
 {
     struct inOut tuple = {"\0", "\0", 0};
     return tuple;
+}
+
+void replacePid(char *target, const char *needle, const char *replacement)
+{
+    char buffer[1024] = {0};
+    char *insert_point = &buffer[0];
+    const char *tmp = target;
+    size_t needle_len = strlen(needle);
+    size_t repl_len = strlen(replacement);
+
+    while (1) 
+    {
+        const char *p = strstr(tmp, needle);
+
+        if (p == NULL) 
+        {
+            strcpy(insert_point, tmp);
+            break;
+        }
+
+        memcpy(insert_point, tmp, p - tmp);
+        insert_point += p - tmp;
+
+        memcpy(insert_point, replacement, repl_len);
+        insert_point += repl_len;
+
+        tmp = p + needle_len;
+    }
+    strcpy(target, buffer);
 }
 
 char *shellUI()
@@ -79,7 +110,8 @@ struct inOut parseSpecialInput(char **fragmentArray)
     }     
     if(strcmp(fragmentArray[i-1], "&") == 0)
     {
-        inOutValues.background = 0;
+        inOutValues.background = 1;
+        printf("hi\n");
     }
     return inOutValues;
 }
@@ -102,12 +134,11 @@ void execCommand(char **fragmentArray)
     char* ch = fragmentArray[i];
     while(strcmp(ch, "\0") != 0)
     {
-        printf("hi");
         ch = fragmentArray[++i];
     }
     fragmentArray[i] = NULL;
     execvp(fragmentArray[0], fragmentArray);
-    perror("execvp");
+    perror("Invalid Input: \n");
 }
 
 
@@ -136,33 +167,7 @@ int cdFunc(char **fragmentArray)
     }
 }
 
-// char **outputIO(char **fragmentArray)
-// {
-//     // if(strcmp(inOutValues.outputFile, "\0") != 0)
-//     // {
-//     //     int targetFD = open(inOutValues.outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0640);
-//     //     if (targetFD == -1) 
-//     //         {
-//     //             perror("open()");
-//     //             exit(1);
-//     //         }
-//     //     // Currently printf writes to the terminal
-//     //     printf("The file descriptor for targetFD is %d\n", targetFD);
-
-//     //     // Use dup2 to point FD 1, i.e., standard output to targetFD
-//     //     int result = dup2(targetFD, 1);
-//     //     if (result == -1) 
-//     //     {
-//     //         perror("dup2"); 
-//     //         exit(2); 
-//     //     }
-//     //     // Now whatever we write to standard out will be written to targetFD
-//     //     fragmentArray[1] = "\0";
-//     // }
-//     return fragmentArray; 
-// }
-
-int forcFunc(char **fragmentArray, struct inOut inOutValues)
+int forcFunc(char **fragmentArray, struct inOut inOutValues, struct sigaction SIGINT_action)
 {
     int status = 0;
     pid_t spawnpid = -5;
@@ -178,51 +183,115 @@ int forcFunc(char **fragmentArray, struct inOut inOutValues)
             exit(1);
             break;
         case 0:
+            SIGINT_action.sa_handler = SIG_DFL;
+            sigaction(SIGINT, &SIGINT_action, NULL);
             if(strcmp(inOutValues.outputFile, "\0") != 0)
             {
                 int targetFD = open(inOutValues.outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0640);
                 if (targetFD == -1) 
                     {
-                        perror("open()");
+                        perror("open()\n");
+                        exit(1);
+                    }
+                // Currently printf writes to the terminal
+                // Use dup2 to point FD 1, i.e., standard output to targetFD
+                int result = dup2(targetFD, 1);
+                if (result == -1) 
+                {
+                    perror("dup2\n"); 
+                    exit(2); 
+                }
+                // Now whatever we write to standard out will be written to targetFD
+                fragmentArray[1] = "\0";
+            }
+            if(strcmp(inOutValues.inputFile, "\0") != 0)
+            {
+                int targetFD = open(inOutValues.inputFile, O_RDONLY, 0640);
+                if (targetFD == -1) 
+                    {
+                        perror("open()\n");
                         exit(1);
                     }
                 // Currently printf writes to the terminal
                 printf("The file descriptor for targetFD is %d\n", targetFD);
 
                 // Use dup2 to point FD 1, i.e., standard output to targetFD
-                int result = dup2(targetFD, 1);
+                int result = dup2(targetFD, 0);
                 if (result == -1) 
                 {
-                    perror("dup2"); 
+                    perror("dup2\n"); 
                     exit(2); 
                 }
                 // Now whatever we write to standard out will be written to targetFD
                 fragmentArray[1] = "\0";
             }
+
+            if(inOutValues.background)
+            {
+                int i = 0;
+                char* ch = fragmentArray[i];
+                while(strcmp(ch, "\0") != 0)
+                {
+                    ch = fragmentArray[++i];
+                }
+                fragmentArray[i-1] = "\0";                 
+            }      
+
             execCommand(fragmentArray);
             status = 1;
             exit(1);
             break;
         default:
-            childPid = waitpid(-1, &childStatus, 0);
-            break;
+            if(inOutValues.background == 0)
+            {
+                childPid = waitpid(-1, &childStatus, 0);
+                break;
+            }
+            if(inOutValues.background == 1)
+            {
+                childPid = waitpid(-1, &childStatus, WNOHANG);              
+                break;
+            }
     }
+
     return status;
 }
 
 int main(int argc, char *argv[])
 {
     int status = 0;
+    // Initialize SIGINT_action struct to be empty
+    struct sigaction SIGINT_action = {0};
+
+    // Fill out the SIGINT_action struct
+    // Register handle_SIGINT as the signal handler
+    SIGINT_action.sa_handler = SIG_IGN;
+    // Block all catchable signals while handle_SIGINT is running
+    sigfillset(&SIGINT_action.sa_mask);
+    // No flags set
+    SIGINT_action.sa_flags = 0;
+
+    // Install our signal handler
+    sigaction(SIGINT, &SIGINT_action, NULL);
+
+    // printf("Send the signal SIGINT to this process by entering Control-C. That will cause the signal handler to be invoked\n");
+    fflush(stdout);
     while(1)
     {
         char **fragmentArray = parseInput(shellUI());
         struct inOut inOutValues;
         inOutValues = parseSpecialInput(fragmentArray);
-        // // printf("%s\n", inOutValues.inputFile); 
-        // // if(strcmp(inOutValues.outputFile, "\0") != 0)
-        // // {
-        // //     printf("hiorigjoikg");
-        // // }
+        int i = 0;
+        char* ch = fragmentArray[i];
+        int pidCount = 0;
+        char pidValue[10000];
+
+        while(strcmp(ch, "\0") != 0)
+        {
+            snprintf(pidValue, sizeof(pidValue), "%d", getpid());
+            replacePid(ch, "$$", pidValue);
+            ch = fragmentArray[++i];
+        }
 
         if(strcmp(fragmentArray[0], "exit") == 0)
         {
@@ -236,22 +305,22 @@ int main(int argc, char *argv[])
         }
         else if(strcmp(fragmentArray[0], "status") == 0)
         {
-            if(WIFEXITED(status))
+            if(WIFSIGNALED(status))
             {
-                printf("WIFStatus %d\n", WIFEXITED(status));
+                printf("WIFStatus %d\n", WIFEXITED(status)); //WTERMSIG
             }
             else
             {
                 printf("Status %d\n", status);
             }
         }
-        else if(strcmp(fragmentArray[0], "#") == 0)
+        else if((fragmentArray[0][0] == '#') != 0)
         {
-            ; //stuffff 
+            ;
         }
         else
         {
-            status = forcFunc(fragmentArray, inOutValues);
+            status = forcFunc(fragmentArray, inOutValues, SIGINT_action);
     
         }
     }
